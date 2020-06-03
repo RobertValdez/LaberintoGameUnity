7176afd3906d66629992f045a4824e945577e07e
Robert <Robert003-@hotmail.com>
2020-06-03 02:30:18 -0400

    Configurando Tiempo y Game Manager

---
+++ Assets/Corex vf/Scripts/Manager/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager sharedInstance_gm;
+
+    Canvas canvasMenuPausa;
+    Camera camara;
+
+    int oldMask;
+
+    void Start()
+    {
+        if (sharedInstance_gm == null)
+        {
+            sharedInstance_gm = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        buscarObjetos();
+    }
+
+    void buscarObjetos()
+    {
+        GameObject canvas = GameObject.Find("Menu de pausa");
+        if (canvas != null)
+        {
+            canvasMenuPausa = canvas.GetComponent<Canvas>();
+            Debug.Log(canvasMenuPausa.name);
+            QuitarPausa();
+        }
+
+        GameObject cam = GameObject.Find("Camera");
+        if (cam != null)
+        {
+            camara = cam.GetComponent<Camera>();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                MostrarMenu(true);
+                oldMask = camara.cullingMask;
+                Time.timeScale = 0;
+
+                camara.clearFlags = CameraClearFlags.Nothing;
+                camara.cullingMask = 0;
+            }
+            else
+            {
+                QuitarPausa();
+            }
+        }
+    }
+
+    public void QuitarPausa()
+    {
+        MostrarMenu(false);
+        Time.timeScale = 1;
+
+        camara.clearFlags = CameraClearFlags.Skybox;
+        camara.cullingMask = oldMask;
+    }
+
+    void MostrarMenu(bool statePause)
+    {
+        canvasMenuPausa.enabled = statePause;
+    }
+}
---
+++ Assets/Corex vf/Scripts/Tiempo/Tiempo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tiempo : MonoBehaviour
+{
+    Text _Text;
+    float timer;
+    private void Awake()
+    {
+        _Text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        int min = (int)(timer / 60) % 60;
+        int seg = (int)(timer % 60);
+
+        string strTime = string.Format("{0:00}:{1:00}", min, seg);
+
+            _Text.text = strTime;
+    }
+}
