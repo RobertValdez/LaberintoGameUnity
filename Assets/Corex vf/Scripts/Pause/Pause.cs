d2f58121ba29ed87f2bf4b47aed05679b25b209d
Robert <Robert003-@hotmail.com>
2020-06-03 02:29:31 -0400

    Configurando UI del Level1, Pausa, Cambio de Niveles y Game Manager

--- Assets/Corex vf/Scripts/Pause/Pause.cs
+++ Assets/Corex vf/Scripts/Pause/Pause.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
-    CameraClearFlags ClearFlags;
+    //public static Pause pauseInstance;
+  /*   public Canvas canvasMenuPausa;
     public Camera camara;
 
     int oldMask;
+
+private void Awake() {
+    pauseInstance = this;
+}
+    private void Start() {
+        canvasMenuPausa.enabled = false;
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (Time.timeScale == 1)
             {
+                MostrarMenu(true);
                 oldMask = camara.cullingMask;
                 Time.timeScale = 0;
 
@@ -22,11 +31,20 @@ public class Pause : MonoBehaviour
             }
             else
             {
-                Time.timeScale = 1;
-                
-                camara.clearFlags = CameraClearFlags.Skybox;
-                camara.cullingMask = oldMask;
+                QuitarPausa();
             }
         }
     }
+
+    public void QuitarPausa(){
+        MostrarMenu(false);
+        Time.timeScale = 1;
+
+        camara.clearFlags = CameraClearFlags.Skybox;
+        camara.cullingMask = oldMask;
+    }
+
+    void MostrarMenu(bool statePause){
+        canvasMenuPausa.enabled = statePause;
+    } */
 }
--- Assets/ScriptMenuPausa/menu de pausa/menupause.cs
+++ Assets/ScriptMenuPausa/menu de pausa/menupause.cs
@@ -5,15 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class menupause : MonoBehaviour
 {
+
     public void OnClicContinuar()
     {
-        
+        GameManager.sharedInstance_gm.QuitarPausa();
     }
     public void OnClicReiniciar()
     {
-
+        SceneManager.LoadScene("Level1");
     }
-    
+
     public void OnClicRegresarMenuInicio()
     {
 
